084f9877d8d6288e0032b190f09f9b4d72ce6018
TCENT\zixuan2.wang <1098958975@qq.com>
2019-01-07 13:41:03 +0800

    webpack配置完毕

--- .webpackrc.js
+++ .webpackrc.js
@@ -1,5 +1,26 @@
-{
-  extraBabelPlugins: [
-    ["import", { "libraryName": "antd", "libraryDirectory": "es", "style": "css" }]
-  ]
-}
+const path = require('path');
+const { resolve } = path
+export default {
+  entry: 'src/index.js',
+  extraBabelPlugins: [['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
+  env: {
+    development: {
+      extraBabelPlugins: ['dva-hmr'],
+      proxy: {
+        "/api": {
+          target: "http://jsonplaceholder.typicode.com/",
+          changeOrigin: true,
+          pathRewrite: { "^/api": "" }
+        },
+      },
+    },
+    production: {
+    }
+  },
+  alias: {
+    '@': resolve(__dirname, 'src'),
+    components: path.resolve(__dirname, 'src/components/')
+  },
+  ignoreMomentLocale: true,
+  hash: true
+};
